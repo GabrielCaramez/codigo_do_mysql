b2cdc367dcc99e4fa6a0bb5dbf9dd3434973eb49
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2024-10-15 16:20:21 -0300

    add new codigos

--- codigo do mysql3.sql
+++ codigo do mysql3.sql
@@ -1,23 +1,44 @@
--- o clientes possui muitos telefones
-alter table `bd_caso_estudo_vendas`.`tb_cli_tel`
-add index `fk_cli_tel_idx`(`tel_cli_cod`asc);
--- Adicionando chave estrangeira
-alter table `bd_caso_estudo_vendas`.`tb_cli_tel`
-add constraint `fk_cli_tel`
-	foreign key (`tel_cli_cod`)
-    references `bd_caso_estudo_vendas`.`tb_cli`(`cli_cod`)
-    on delete no action
-    on update no action;
--- compras possuem muitos produtos
-alter table `bd_caso_estudo_vendas`. `tb_prod_comp`
-add index `fk_prod_comp_compra_idx`(`compra_cod`asc);
--- add a chave estrangeira
-alter table `bd_caso_estudo_vendas`.`tb_prod_comp`
-add constraint `fk_prod_comp_compra`
-	foreign key(`compra_cod`)
-    references `bd_caso_estudo_vendas`.`tb_compra`(`compra_cod`)
-    on delete no action 
-    on update no action;
-    
+create user 'lara.rodrigues'@'localhost'identified by'123456';
+create user 'izabella.oliveira'@'localhost' identified by '123456';
+create user 'luane.gabrielly'@'localhost' identified by '123456';
+create user 'tiago,lima'@'localhost' identified by '123456';
+create user 'danielgomes'@'localhost' identified by '123456';
 
-
+grant insert, update, delete
+ON `bd_caso_estudo_vendas`.`tb_func`
+TO `hudson_silva@localhost`;
+/*Removendo permissão concedida*/
+revoke insert, update
+ON `bd_caso_estudo_vendas`.`tb_func`
+from `hudson_silva@localhost`;
+/*Gerenciamento de permissões e usuários, concedendo permissão para
+remover registros em uma tabela especifica*/
+grant delete
+ON `bd_caso_estudo_vendas`.`tb_func`
+TO `hudson_silva@localhost`;
+/*Removendo permissão de consulta*/
+revoke select
+ON `bd_caso_estudo_vendas`.`tb_func`
+from `hudson_silva@localhost`;
+/*Gerenciamento de permissões e usuários, concedendo permissão para
+inserir registros em uma tabela especifica*/
+grant insert
+ON `bd_caso_estudo_vendas`.`tb_func`
+TO `hudson_silva@localhost`;
+/*Aplicando permissão global de consulta a todos os arquivos/banco no servidor*/
+grant select
+on *.*
+to danielgomes@localhost;
+/*Aplicando permissão de banco inserção em todas as tabelas do banco*/
+grant insert
+on bd_caso_estudo_vendas.*
+to danielgomes@localhost;
+/*Aplicando permissão de banco de inserção em tabela definida*/
+grant insert
+on bd_caso_estudo_vendas.tb_prod
+to danielgomes@localhost;
+grant
+	select (cli_cod,cli_nome, cli_cpf),
+    update(cli_end_cep)
+on bd_caso_estudo_vendas.tb_cli
+to danielgomes@localhost;
---
+++ codigo do mysql6.sql
@@ -0,0 +1,65 @@
+set foreign_key_checks=0;
+insert into `bd_caso_estudo_vendas`.`tb_forn`(forn_cod, forn_nome, forn_tel)
+values
+(3, 'Fornecedor três', '113333333'),
+(4, 'Fornecedor quatro', '114444444'),
+(5, 'Fornecedor cinco', '115555555'),
+(6, 'Fornecedor seis', '116666666'),
+(7, 'Fornecedor sete', '117777777'),
+(8, 'Fornecedor oito', '118888888'),
+(9, 'Fornecedor nove', '119999999'),
+(10, 'Fornecedor dez', '120000000'),
+(11, 'Fornecedor onze', '121111111'),
+(12, 'Fornecedor doze', '122222222'),
+(13, 'Fornecedor treze', '123333333'),
+(14, 'Fornecedor quatorze', '124444444'),
+(15, 'Fornecedor quinze', '125555555'),
+(16, 'Fornecedor dezesseis', '126666666'),
+(17, 'Fornecedor dezessete', '127777777'),
+(18, 'Fornecedor dezoito', '128888888'),
+(19, 'Fornecedor dezenove', '129999999'),
+(20, 'Fornecedor vinte', '130000000'),
+(21, 'Fornecedor vinte e um', '131111111'),
+(22, 'Fornecedor vinte e dois', '132222222'),
+(23, 'Fornecedor vinte e três', '133333333'),
+(24, 'Fornecedor vinte e quatro', '134444444'),
+(25, 'Fornecedor vinte e cinco', '135555555'),
+(26, 'Fornecedor vinte e seis', '136666666'),
+(27, 'Fornecedor vinte e sete', '137777777'),
+(28, 'Fornecedor vinte e oito', '138888888'),
+(29, 'Fornecedor vinte e nove', '139999999'),
+(30, 'Fornecedor trinta', '140000000'); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
