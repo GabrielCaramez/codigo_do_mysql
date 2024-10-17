db3e44f8bf0446d6f63b086aecad0ba72a67698c
GABRIEL CARAMEZ BENVINDO DA SILVA <155910430+GabrielCaramez@users.noreply.github.com>
2024-10-17 16:59:57 -0300

    update from table

--- codigo do mysql7.sql
+++ codigo do mysql7.sql
@@ -45,3 +45,30 @@ select `func_nome`,`depto_desc`
 from `bd_caso_estudo_vendas`.`tb_func`
 join `bd_caso_estudo_vendas`.`tb_depto`
 on `tb_func`.`func_depto` = `tb_depto`.`depto_cod`;
+
+select * from `bd_caso_estudo_vendas`.`tb_func`
+inner join `bd_caso_estudo_vendas`.`tb_depto`
+	on depto_cod = func_depto;
+    
+select cli_nome, prod_desc
+from tb_cli
+join tb_compra
+on tb_cli.cli_cod = tb_compra.compra_cli_cod
+join tb_prod_comp
+on tb_compra.compra_cod = tb_prod_comp.compra_cod
+join tb_prod
+on tb_prod_comp.prod_comp = tb_prod.prod_cod;
+
+create view `VIEW_COMPRA_PRODUTO`
+AS
+select tb_compra.compra_cod, prod_desc,cli_nome
+from `bd_caso_estudo_vendas`.`tb_compra`
+inner join bd_caso_estudo_vendas.tb_prod_comp
+on tb_compra.compra_cod = tb_prod_comp.compra_cod
+inner join bd_caso_estudo_vendas.tb_prod
+on tb_prod.prod_cod = tb_prod_comp.prod_cod
+inner join bd_caso_estudo_vendas.tb_cli
+on compra_cli_cod = cli_cod;
+
+
+select * from view_compra_produto;
